cbc4fb1384746805a85c1eb3fd77c8e890f7f6d6
Nurgissa362 <arisnurgysa@icloud.com>
2025-01-29 10:35:20 +0500

    Message

--- lab3/1.py
+++ lab3/1.py
@@ -1,1 +1,18 @@
-# hello
+# Classes
+# 1.
+
+class StringManipulator:
+    def __init__(self):
+        self.text = ""
+
+    def getString(self):
+        self.text = input("Введите строку: ")
+
+    def printString(self):
+        print(self.text.upper())
+
+
+string_obj = StringManipulator()
+string_obj.getString()
+string_obj.printString()
+
