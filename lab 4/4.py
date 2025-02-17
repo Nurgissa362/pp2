1d4aa754624341779ab3d9aa3639cb0485dc5be0
Nurgissa362 <arisnurgysa@icloud.com>
2025-02-17 18:32:39 +0500

    done for 80 percent

--- lab 4/4.py
+++ lab 4/4.py
@@ -1,135 +1,130 @@
 #4th lab
-#classes
+#date
 #1
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+new_date = current_date - timedelta(days=5)
+print(new_date.date())
 
 
-class StringHandler:
-    def __init__(self):
-        self.text = ""
-
-    def getString(self):
-        self.text = input("Enter a string: ")
-
-    def printString(self):
-        print(self.text.upper())
-
-
-obj = StringHandler()
-obj.getString()
-obj.printString()
-
-#2
-
-class Shape:
-    def area(self):
-        return 0
-
-class Square(Shape):
-    def __init__(self, length):
-        self.length = length
-
-    def area(self):
-        return self.length ** 2
-
-
-sq = Square(5)
-print(sq.area()) 
-
-
-#3
-
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-
-rect = Rectangle(4, 6)
-print(rect.area()) 
-
-
-#functions1
-#1
-
-
-def grams_to_ounces(grams):
-    return 28.3495231 * grams
-
-
-print(grams_to_ounces(100))  
 
 
 #2
+from datetime import datetime, timedelta
 
-def fahrenheit_to_celsius(fahrenheit):
-    return (5 / 9) * (fahrenheit - 32)
+today = datetime.now().date()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
 
 
-print(fahrenheit_to_celsius(100))  
 
 #3
-def solve(numheads, numlegs):
-    for chickens in range(numheads + 1):
-        rabbits = numheads - chickens
-        if (chickens * 2 + rabbits * 4) == numlegs:
-            return chickens, rabbits
-    return "No solution"
+from datetime import datetime
+
+date1 = datetime(2024, 2, 10, 12, 0, 0)
+date2 = datetime(2024, 2, 12, 14, 30, 0)
+
+difference = (date2 - date1).total_seconds()
+print(difference)
 
 
-print(solve(35, 94))  
+#4
+from datetime import datetime
+
+date1 = datetime(2024, 2, 10, 12, 0, 0)
+date2 = datetime(2024, 2, 12, 14, 30, 0)
+
+difference = (date2 - date1).total_seconds()
+print(difference)
 
 
-#functions2
+
+
+
+#generators
 #1
+def generate_squares(N):
+    for i in range(N + 1):
+        yield i ** 2
 
-class Movie:
-    def __init__(self, name, imdb, category):
-        self.name = name
-        self.imdb = imdb
-        self.category = category
-
-    def is_highly_rated(self):
-        return self.imdb > 5.5
-
-movie1 = Movie("Hitman", 6.3, "Action")
-print(movie1.is_highly_rated())  
+for num in generate_squares(5):
+    print(num)
 
 
 #2
+def even_numbers(n):
+    for i in range(0, n + 1, 2):
+        yield i
 
-class MovieDatabase:
-    def __init__(self, movies):
-        self.movies = [Movie(m["name"], m["imdb"], m["category"]) for m in movies]
-
-    def highly_rated_movies(self):
-        return list(filter(lambda movie: movie.is_highly_rated(), self.movies))
-
-
-movies_data = [
-    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
-    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
-    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
-    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"}
-]
+n = int(input("Enter n: "))
+print(",".join(map(str, even_numbers(n))))
 
 
-db = MovieDatabase(movies_data)
-
-
-print([movie.name for movie in db.highly_rated_movies()]) 
 
 
 #3
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
 
-class MovieDatabase:
-    def __init__(self, movies):
-        self.movies = [Movie(m["name"], m["imdb"], m["category"]) for m in movies]
-
-    def movies_by_category(self, category):
-        return list(filter(lambda movie: movie.category == category, self.movies))
+for num in divisible_by_3_and_4(50):
+    print(num)
 
 
-print([movie.name for movie in db.movies_by_category("Romance")]) 
+
+#4
+
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i ** 2
+
+for num in squares(3, 7):
+    print(num)
+
+#5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
+
+
+for num in countdown(5):
+    print(num)
+
+
+
+#math
+#1
+import math
+
+def degree_to_radian(degree):
+    return round(degree * (math.pi / 180), 6)
+
+print(degree_to_radian(15)) 
+
+
+#2
+def trapezoid_area(height, base1, base2):
+    return ((base1 + base2) * height) / 2
+
+print(trapezoid_area(5, 5, 6))  
+
+
+ #3
+import math
+
+def polygon_area(sides, length):
+    return (sides * (length ** 2)) / (4 * math.tan(math.pi / sides))
+
+print(polygon_area(4, 25)) 
+
+
+#4
+def parallelogram_area(base, height):
+    return base * height
+print(parallelogram_area(5, 6))  
