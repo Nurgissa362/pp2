65d67472a90fcae0b70406020826e5cbdc2596cb
Nurgissa362 <arisnurgysa@icloud.com>
2025-02-24 21:35:35 +0500

    21:34 monday

--- lab 5/5.py
+++ lab 5/5.py
@@ -1,65 +1,66 @@
-#1
+import re
+
+#1.
 def match_a_b(s):
     return bool(re.fullmatch(r'ab*', s))
 
-#2
+print(match_a_b("a"))  #True
+print(match_a_b("abbb"))  #True
+print(match_a_b("b"))  #False
+
+#2. 
 def match_a_b_2_3(s):
     return bool(re.fullmatch(r'ab{2,3}', s))
 
-#3
+print(match_a_b_2_3("abb"))  #True
+print(match_a_b_2_3("abbb"))  #True
+print(match_a_b_2_3("abbbb"))  #False
+
+#3.
 def find_lower_underscore(s):
     return re.findall(r'\b[a-z]+_[a-z]+\b', s)
 
-#4
+print(find_lower_underscore("hello_world test_text example_case"))
+
+#4. 
+
 def find_upper_lower(s):
     return re.findall(r'\b[A-Z][a-z]*\b', s)
 
-#5
+print(find_upper_lower("Hello World From Python"))
+
+#5. 
 def match_a_any_b(s):
     return bool(re.fullmatch(r'a.*b', s))
 
-#6
-import re
+print(match_a_any_b("acb"))  #True
+print(match_a_any_b("a123b"))  #True
+print(match_a_any_b("abc"))  #False
 
+#6. 
 text = "Hello, how are you? I am fine. Thank you."
 result = re.sub(r'[ ,.]', ':', text)
 print(result)
 
-#7
+#7. 
 def snake_to_camel(s):
     words = s.split('_')
     return words[0] + ''.join(word.capitalize() for word in words[1:])
 
-snake_str = "hello_world_example"
-camel_str = snake_to_camel(snake_str)
-print(camel_str)
+print(snake_to_camel("hello_world_example"))
 
-#8
-import re
-
+#8. 
 text = "SplitAtUpperCaseLetters"
-result = re.findall(r'[A-Z][a-z]*', text)
-print(result)
+print(re.findall(r'[A-Z][a-z]*', text))
 
-
-#9
-import re
-
+#9. 
 def insert_spaces(s):
     return re.sub(r'([A-Z])', r' \1', s).strip()
 
-text = "InsertSpacesBetweenWords"
-result = insert_spaces(text)
-print(result)
+print(insert_spaces("InsertSpacesBetweenWords"))
 
-#10
-import re
-
+#10. 
 def camel_to_snake(s):
     return re.sub(r'(?<!^)([A-Z])', r'_\1', s).lower()
 
-camel_str = "CamelCaseToSnake"
-snake_str = camel_to_snake(camel_str)
-print(snake_str)
-
-
+print(camel_to_snake("CamelCaseToSnake"))
